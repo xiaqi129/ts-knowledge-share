753b8e5902c65c048a8ef2e08684275e56d271d4
xiaqi <xiaqi@cisco.com>
2019-11-14 10:23:46 +0800

    upddate

--- js/sortByName.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function sortName(a) {
-    var result = a.slice(0);
-    result.sort(function (x, y) {
-        return x.name.localeCompare(y.name);
-    });
-    return result;
-}
-sortName([]);
