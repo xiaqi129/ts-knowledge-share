9de8e511086a25aa291a8ef414d6e9f1cd003f33
xiaqi <xiaqi@cisco.com>
2020-03-23 15:10:28 +0800

    update

--- sortByName.js
+++ sortByName.js
@@ -1,3 +1,12 @@
+
+/*
+    阅读下面这个JS方法，我们知道它获得一个参数a。
+    那么关于这个参数a的任何信息，我们都不清楚。
+    我们只能通过去阅读这个方法，来推测这个参数a的类型格式。
+
+*/
+
+
 function sortName(a) {
     var result = a.slice(0);
     result.sort(function (x, y) {
--- sortByName.ts
+++ sortByName.ts
@@ -1,9 +1,21 @@
-interface People {
+
+/*
+
+    阅读下面ts代码，我们看到我们为参数a明确了类型。
+    参数a类型映射为一个Person的接口，其中必须有name和age参数。
+    且name参数类型为string，age参数类型为number。
+    通过这样明确的类型定义，我们不去看方法体，就能了解方法的作用已经传参方式。
+    这样可以在编译前，更轻松的找到我们代码种错误所在。
+
+*/
+
+
+interface Person {
     name: string;
     age: number;
 }
 
-function sortName(a: People[]) {
+function sortName(a: Person[]) {
     var result = a.slice(0);
     result.sort(function (x, y) {
         return x.name.localeCompare(y.name);
